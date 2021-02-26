f73925de0b7bc63dd199f7a9904e3d2806acedca
mj <mu86@naver.com>
2021-02-26 17:55:15 +0900

    first commit

---
+++ app/views.py
@@ -0,0 +1,66 @@
+from app import app
+
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template('public/index.html')
+
+
+@app.route("/about")
+def about():
+    return """
+    <h1 style='color: red;'>I'm a red H1 heading!</h1>
+    <p>This is a lovely little paragraph</p>
+    <code>Flask is <em>awesome</em></code>
+    """
+
+
+@app.route("/jinja")
+def jinja():
+    # Strings
+    my_name = "Julian"
+
+    # Integers
+    my_age = 30
+
+    # Lists
+    langs = ["Python", "JavaScript", "Bash", "Ruby", "C", "Rust"]
+
+    # Dictionaries
+    friends = {
+        "Tony": 43,
+        "Cody": 28,
+        "Amy": 26,
+        "Clarissa": 23,
+        "Wendell": 39
+    }
+
+    # Tuples
+    colors = ("Red", "Blue")
+
+    # Booleans
+    cool = True
+
+    # Classes
+    class GitRemote:
+        def __init__(self, name, description, domain):
+            self.name = name
+            self.description = description
+            self.domain = domain
+
+        def clone(self, repo):
+            return f"Cloning into {repo}"
+
+    my_remote = GitRemote(
+        name="Learning Flask",
+        description="Learn the Flask web framework for Python",
+        domain="https://github.com/Julian-Nash/learning-flask.git"
+    )
+
+    # Functions
+    def repeat(x, qty=1):
+        return x * qty
+
+    return render_template("public/jinja.html", my_name=my_name)
